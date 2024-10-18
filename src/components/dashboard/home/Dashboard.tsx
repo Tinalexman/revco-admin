260fdddbb2f855c5e002957a6be4a436a2340a15
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-10-18 13:34:15 +0100

    Updated the admin dashboard

--- src/components/dashboard/home/Dashboard.tsx
+++ src/components/dashboard/home/Dashboard.tsx
@@ -7,14 +7,13 @@ import Activity from "../common/Activity";
 import Statistics from "./statistics/Statistics";
 
 const Dashboard = () => {
-  const [state, setState] = useState<string>("Taraba");
 
   return (
     <div className="w-full flex flex-col">
       <div className="px-8 bg-white w-full h-[4.5rem]">
         <div className="w-full flex flex-col justify-around py-2">
           <h2 className="text-monokai font-semibold text-dash-intro-header">
-            Welcome to {state} State Revenue Board Admin Portal
+            Welcome to Taraba State Board of Internal Revenue Board Admin Portal
           </h2>
           <h3 className="text-primary text-reg-caption">
             Manage all transactions and data on the Revco service
