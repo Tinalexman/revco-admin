6c18183853b5e84fa4401e01c86a5b21a81b3be0
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-11-08 20:21:08 +0100

    Fixed the register issue

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,7 +1,7 @@
-// import Register from "@/components/auth/register/Register";
+import Register from "@/components/auth/register/Register";
 
-// export const metadata = {
-//   title: "Register",
-// };
+export const metadata = {
+  title: "Register",
+};
 
-// export default Register;
+export default Register;
